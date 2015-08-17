ac017b4a78450f0824259e35a2234ea3940223ba
vinayg <vinay.gurram@olacabs.com>
2015-08-17 13:47:36 +0530

    Mon Aug 17 13:47:36 IST 2015

--- src/main/java/clusters/updates/redis/Publisher.java
+++ src/main/java/clusters/updates/redis/Publisher.java
@@ -10,28 +10,28 @@ public class Publisher
 
     public Publisher(Jedis publisherJedis)
     {
-           this.publisherJedis = publisherJedis;
+        this.publisherJedis = publisherJedis;
     }
     public void start(String channel_name)
     {
         System.out.println("Type your message....exit for terminate");
         try
-      {
+        {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-  
-             while (true)
-             {
-                  String line = reader.readLine();
-                  if (!"exit".equals(line)) {
-                     publisherJedis.publish(channel_name, line);
-                    }
-            else {
-                            break;
-                      }
+
+            while (true)
+            {
+                String line = reader.readLine();
+                if (!"exit".equals(line)) {
+                    publisherJedis.publish(channel_name, line);
                 }
+                else {
+                    break;
+                }
+            }
         }
-    catch (IOException e) {
-                System.out.println("IO failure while reading input, e");
+        catch (IOException e) {
+            System.out.println("IO failure while reading input, e");
         }
     }
 }
